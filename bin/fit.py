1b316f9d0c7d641fd6196e69dba2035bddedd916
aegroto <aegroto@protonmail.com>
2024-09-30 12:10:35 +0200

    chore: args and log level from env

--- bin/fit.py
+++ bin/fit.py
@@ -5,9 +5,19 @@ from modules.logging import setup_logging
 
 LOGGER = logging.getLogger(__name__)
 
+def __load_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_path", type=str)
+    return parser.parse_args()
+
+
 def main():
     setup_logging()
     LOGGER.info("Running")
+
+    args = __load_args()
+
+    LOGGER.debug(f"Command-line args: {args}")
 
 if __name__ == "__main__":
     main()
--- modules/logging.py
+++ modules/logging.py
@@ -1,5 +1,15 @@
 import logging
+import os
+
+def __env_logging_level():
+    try:
+        return os.environ['LOG_LEVEL']
+    except:
+        return 'INFO'
 
 def setup_logging():
-    logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(asctime)s - %(name)s: %(message)s")
+    logging.basicConfig(
+        level=__env_logging_level(), 
+        format="[%(levelname)s] %(asctime)s - %(name)s: %(message)s"
+    )
 
