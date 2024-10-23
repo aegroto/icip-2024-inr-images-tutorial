37950ede67532b83800a5cd473c70e2392438a78
aegroto <aegroto@protonmail.com>
2024-10-23 09:24:17 +0200

    feat: removed implementation code

--- bin/encode.py
+++ bin/encode.py
@@ -1,10 +1,4 @@
 import argparse
-from bin.fit import fit
-from bin.pack import pack
-from modules.data import ImageData
-from modules.device import load_device
-from modules.helpers.config import load_config
-from modules.helpers.reproducibility import load_seed_from_env
 from modules.logging import init_logger, setup_logging
 
 LOGGER = init_logger(__name__)
@@ -12,9 +6,6 @@ LOGGER = init_logger(__name__)
 
 def __load_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("image_path", type=str)
-    parser.add_argument("output_path", type=str)
-    parser.add_argument("--config", type=str, required=False, default="default")
     return parser.parse_args()
 
 
@@ -23,19 +14,7 @@ def main():
     args = __load_args()
     LOGGER.debug(f"Command-line args: {args}")
 
-    config = load_config(args.config)
-    device = load_device()
-
-    image_data = ImageData(args.image_path, device)
-
-    encode(config, image_data, args.output_path, device)
-
-
-def encode(config, image_data, output_path, device):
-    load_seed_from_env()
-    fitted_state_dict = fit(config, image_data, device)
-    pack(config, fitted_state_dict, output_path, device)
-
+    LOGGER.warning("Not implemented yet")
 
 if __name__ == "__main__":
     main()
--- modules/device/__init__.py
+++
@@ -1,9 +0,0 @@
-import torch
-import os
-
-
-def load_device(force_cpu=False):
-    if force_cpu:
-        torch.device("cpu")
-    else:
-        return torch.device(os.environ.get("DEVICE") or "cuda")
